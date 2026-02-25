f9748e870e0c81931be0a835a31ac2ac25af0d0c
njosep17 <nithin_joseph@optum.com>
2026-02-25 09:01:35 -0600

    Reorder homepage sections: move Our Story after Our Favorites

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
-      <AboutUsSection />
       <FeaturedProducts />
+      <AboutUsSection />
     </div>
   );
 }
